09496e54cd0e21379c934077b5ef44b44dfa747b
Virendra <parmarvirendraujala@gmail.com>
2026-06-29 08:43:20 +0530

    Media

---
+++ JS/variables/script.js
@@ -0,0 +1,15 @@
+// Variables in js
+var a = 12;
+// console.log(a)
+
+//using var keyword you can reinitialise and redeclare a varaible in js
+var a = 14;
+// console.log(a);
+
+let b = 12;
+// let b = 14;
+
+
+const PI = 3.14;
+PI = 3;
+console.log(PI);
