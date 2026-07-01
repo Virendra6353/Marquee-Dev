28ac5feed2f56bcee4ac9cd55349203eea0c6093
Virendra <parmarvirendraujala@gmail.com>
2026-07-01 13:48:41 +0530

    JS

---
+++ JS/operations/string.js
@@ -0,0 +1,17 @@
+let str = "HEllo world"
+
+console.log(str)
+console.log(str.length)
+console.log("\*Hello \* world")
+console.log(str.charAt(0));
+console.log(str.indexOf("w"));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.substring(0,5));
+console.log(str.slice(-5));
+console.log(str.substr(1,6));
+
+let s = "    username   "
+console.log(s.trim());
+console.log(str.replace("HEllo","Bye"));
+console.log(str.includes("Hello"))
