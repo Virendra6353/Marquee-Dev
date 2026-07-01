78d41f4507dc35ca88b3403c278ab66418ef4c22
Virendra <parmarvirendraujala@gmail.com>
2026-07-01 12:39:40 +0530

    Media

---
+++ JS/operations/loop.js
@@ -0,0 +1,15 @@
+let n = 10
+for(let i=0;i<n;i++){
+    console.log(i);
+}
+
+let m = 8
+while(m > 0){
+    console.log(m)
+    m--;
+}
+m = 1
+do{
+    console.log("America aya")
+    m--;
+}while(m > 0)
