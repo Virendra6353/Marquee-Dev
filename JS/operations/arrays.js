4200a6f54de0548b45b9a649445a578fef004569
Virendra <parmarvirendraujala@gmail.com>
2026-07-06 11:18:05 +0530

    Array

--- JS/operations/arrays.js
+++ JS/operations/arrays.js
@@ -1,7 +1,13 @@
 arr =[1,2,3,4]
-arr.shift()
-console.log(arr);
-arr.unshift(1)
-for(let i in arr){
-    console.log(i+" "+arr[i]);   
-}
+// arr.shift()
+// console.log(arr);
+// arr.unshift(1)
+// for(let i in arr){
+//     console.log(i+" "+arr[i]);   
+// 
+
+let total = arr.reduce((i,j)=>{
+    return i+j;
+})
+
+console.log(total);
---
+++ JS/operations/calc.js
@@ -0,0 +1,26 @@
+x=10
+y=5
+
+
+
+function calc(a,b,op){
+    op(a,b)
+}
+
+function add(a,b){
+    console.log(a+b)
+}
+
+function sub(a,b){
+    console.log(a-b);
+}
+
+function mult(a,b){
+    console.log(a*b);
+}
+
+function divi(a,b){
+    console.log(a/b);
+}
+
+calc(x,y,mult)
